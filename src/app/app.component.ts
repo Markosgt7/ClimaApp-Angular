dbed91a1afd2c2e81970b285a376ef3a460cdf12
Markosgt7 <markos.guate7@gmail.com>
2023-08-16 12:06:40 -0600

    creacion del componente de clima

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Aplicación del clima - by Markos';
+
+  constructor(private weatherService: WeatherService) {}
+  ngOnInit() {
+    /*   this.weatherService.getWeather('london', 'uk').subscribe(
+      (res) => console.log(res),
+      (err) => console.log(res) */
+  }
 }
